b7e42cb22bc2b15489ac61287835b606490fcf7c
vladflip <vlad.flip.prg@gmail.com>
2014-07-20 09:51:19 +0300

    perfect

--- portfolio2/js/script.js
+++ portfolio2/js/script.js
@@ -12,11 +12,19 @@
   				$("#slidernews").owlCarousel({
 					  navigation : true, // Show next and prev buttons
 					  singleItem:true,
+            transitionStyle:"goDown",
+            navigationText: ["",""],
 					  paginationSpeed : 2000,
-					  autoPlay:4000,
 					  lazyLoad:true
   			});
-
+          $("#bottomslider").owlCarousel({
+            navigation : true, // Show next and prev buttons
+            singleItem:true,
+            paginationSpeed : 2000,
+            transitionStyle: "fade",
+            autoPlay:4000,
+            lazyLoad:true
+          });
   			// 	$("#slidertop").zAccordion({
   			// 		trigger:'click',
   			// 		easing: 'linear',
@@ -39,53 +47,57 @@
   			// });
 
 // var div = document.getElementById("sort");
-// var menu = $("#menu ul li");
-// 		menu[0].onclick= function(){
-// 			$('html,body').animate({
-//           		scrollTop: document.getElementById("header").offsetTop - 79
-//         	}, 1000);
 
-// 		}
+var header = document.getElementById("header");
+var news = document.getElementById("newsblock");
+var bio = document.getElementById("bioblock");
+var portfolio = document.getElementById("portfolio");
+var footer = document.getElementById("footer");
+var menu = $("#header li");
+		menu[0].onclick= function(){
+			$('html,body').animate({
+          		scrollTop: header.offsetTop - 20
+        	}, 1000);
 
-// 		menu[1].onclick= function(){
-// 			$('html,body').animate({
-//           		scrollTop: document.getElementById("bio").offsetTop - 79
-//         	}, 1000);
+		}
 
-
-// 		}
-
-// 		menu[2].onclick= function(){
-// 			$('html,body').animate({
-//           		scrollTop: document.getElementById("model").offsetTop - 79
-//         	}, 1000);
+		menu[1].onclick= function(){
+			$('html,body').animate({
+          		scrollTop: news.offsetTop - 10
+        	}, 1000);
 
 
-// 		}
+		}
 
-// 		menu[3].onclick= function(){
-// 			$('html,body').animate({
-//           		scrollTop: document.getElementById("footer").offsetTop - 79
-//         	}, 1000);
+		menu[2].onclick= function(){
+			$('html,body').animate({
+          		scrollTop: bio.offsetTop - 10
+        	}, 1000);
 
 
-// 		}
+		}
 
-// var header = document.getElementById("header");
-// var bio = document.getElementById("bio");
-// var model = document.getElementById("model");
-// var footer = document.getElementById("footer");
-// window.onscroll = function(){
-// 	var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+		menu[3].onclick= function(){
+      $('html,body').animate({
+              scrollTop: portfolio.offsetTop - 10
+          }, 1000);
+}
+    menu[4].onclick= function(){
+			$('html,body').animate({
+          		scrollTop: footer.offsetTop - 10
+        	}, 1000);
+
+
+		}
+
+
+window.onscroll = function(){
+	var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
 	
-// 	if(scrollTop<bio.offsetTop-2) div.style.marginLeft = '0px';
-// 	if(scrollTop>bio.offsetTop-100&&scrollTop<model.offsetTop) div.style.marginLeft = '90px';
-// 	if(scrollTop>model.offsetTop-100&&scrollTop<footer.offsetTop) div.style.marginLeft = '175px';
-// 	if(scrollTop>model.offsetTop+20) div.style.marginLeft = '255px';
-// }
+	// if(scrollTop<bio.offsetTop-2) div.style.marginLeft = '0px';
+	// if(scrollTop>bio.offsetTop-100&&scrollTop<model.offsetTop) div.style.marginLeft = '90px';
+	// if(scrollTop>model.offsetTop-100&&scrollTop<footer.offsetTop) div.style.marginLeft = '175px';
+	// if(scrollTop>model.offsetTop+20) div.style.marginLeft = '255px';
+}
 
-// var next = document.getElementsByClassName("owl-next")[0];
-// var prev = document.getElementsByClassName("owl-prev")[0];
 
-// next.innerHTML = '';
-// prev.innerHTML = '';
