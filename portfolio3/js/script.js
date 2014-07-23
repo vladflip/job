ce75f5f27e0f4c69b4671d98496afd6e14121fc6
vladflip <vlad.flip.prg@gmail.com>
2014-07-23 15:20:10 +0300

    sdf

--- portfolio3/js/script.js
+++ portfolio3/js/script.js
@@ -1,26 +1,25 @@
 $(document).ready(function(){
-            $("#modelslider").owlCarousel({
-            animateOut: 'fadeOutDown',
-            items:1,
-            margin:400,
-            stagePadding:30,
-            smartSpeed:450,
-            autoplay:true,
-            autoWidth:true,
-            loop:true,
-            margin:150
-        });
-          // $("#bottomslider").owlCarousel({
-          //   animateOut: 'fadeOutDown',
-          //   items:1,
-          //   margin:200,
-          //   stagePadding:30,
-          //   smartSpeed:450,
-          //   nav:true,
-          //   autoWidth:true,
-          //   autoplay:true,
-          //   navText:["",""],
-          //   loop:true,
-          //   margin:150
-          // });
+		$("#modelslider").owlCarousel({
+			animateOut: 'fadeOutDown',
+			items:1,
+			margin:400,
+			stagePadding:30,
+			smartSpeed:450,
+			autoplay:true,
+			autoWidth:true,
+			loop:true,
+			margin:150
+		});
+		$("#actressslider").owlCarousel({
+			animateOut: 'fadeOutDown',
+			items:3,
+			margin:30,
+			stagePadding:30,
+			smartSpeed:450,
+			nav:true,
+			autoWidth:true,
+			autoplay:true,
+			navText:["next","prev"],
+			loop:true
+		});
 });
