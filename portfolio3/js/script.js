634bdc0619057ab0a7de35693416986b2ade90a9
vladflip <vlad.flip.prg@gmail.com>
2014-07-23 05:36:19 +0300

    home

---
+++ portfolio3/js/script.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+            $("#modelslider").owlCarousel({
+            animateOut: 'fadeOutDown',
+            items:1,
+            margin:400,
+            stagePadding:30,
+            smartSpeed:450,
+            nav:true,
+            autoWidth:true,
+            //autoplay:true,
+            navText:["next","prev"],
+            loop:true,
+            margin:150
+        });
+          // $("#bottomslider").owlCarousel({
+          //   animateOut: 'fadeOutDown',
+          //   items:1,
+          //   margin:200,
+          //   stagePadding:30,
+          //   smartSpeed:450,
+          //   nav:true,
+          //   autoWidth:true,
+          //   autoplay:true,
+          //   navText:["",""],
+          //   loop:true,
+          //   margin:150
+          // });
+});
